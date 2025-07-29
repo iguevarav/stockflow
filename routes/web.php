59ed5818cfe829a611ac272b9fd449747bf4726a
iguevarav <iguevara@unitru.edu.pe>
2025-07-29 13:46:34 -0500

    cambios en rutas / funciona correctamente

--- routes/web.php
+++ routes/web.php
@@ -5,17 +5,21 @@ use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\ProductoController;
 use App\Http\Controllers\CategoriaController;
 use App\Http\Controllers\MovimientoInventarioController;
-use App\http\Controllers\DashboardController;
-
-Auth::routes();
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
 
 Route::get('/', function () {
     return Auth::check() ? redirect()->route('dashboard') : redirect()->route('login');
 });
 
-// Route::get('/', function () {
-//     return redirect()->route('dashboard');
-// });
+// Rutas de autenticación
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [LoginController::class, 'login']);
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+
+Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
+Route::post('/register', [RegisterController::class, 'register']);
 
 Route::middleware('auth')->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
